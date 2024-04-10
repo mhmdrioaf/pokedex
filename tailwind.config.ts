fc0de5987261ea60bf7f9e38d657624c3b0a4eb8
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-04-10 22:08:34 +0700

    chore: initializing UI color palettes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,17 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        background: "rgb(var(--background) / <alpha-value>)",
+        foreground: "rgb(var(--foreground) / <alpha-value>)",
+        primary: "rgb(var(--primary) / <alpha-value>)",
+        "primary-foreground": "rgb(var(--primary-foreground) / <alpha-value>)",
+        accent: "rgb(var(--accent) / <alpha-value>)",
+        "accent-foreground": "rgb(var(--accent-foreground) / <alpha-value>)",
+      },
+      padding: {
+        "container-horizontal": "var(--padding-container-horizontal)",
+        "container-vertical": "var(--padding-container-vertical)",
       },
     },
   },
