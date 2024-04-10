a9c501b193480b515767ad649fddff2df80bb8a2
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-04-10 22:14:58 +0700

    feat(pokemon-list): implementing the react-query provider and pokemon context provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/ui/header";
+import Provider from "@/lib/query-provider";
+import { PokemonContextProvider } from "@/lib/context/context";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        <Provider>
+          <PokemonContextProvider>{children}</PokemonContextProvider>
+        </Provider>
+      </body>
     </html>
   );
 }
