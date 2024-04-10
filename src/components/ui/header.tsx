717b6319fbf328caa56fbcd024eabc82db89bcb2
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-04-10 22:13:33 +0700

    chore: initializing the header component

---
+++ src/components/ui/header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <section
+      id="sec__header"
+      className="w-full flex flex-col gap-2 items-center justify-center bg-accent text-accent-foreground px-container-horizontal py-4"
+    >
+      <h2 className="text-4xl font-bold">Pokedex</h2>
+    </section>
+  );
+}
