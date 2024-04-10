0358edac7acf84f27601fa2eab3d8b117cda5ffb
Muhammad Rio Ananta Firdaus <dev.rioananta@gmail.com>
2024-04-11 04:50:57 +0700

    chore: adding loading indicator

---
+++ src/app/loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <main className="w-full h-svh flex items-center justify-center">
+      <img
+        src="/pokeball.png"
+        className="w-16 h-16 animate-spin"
+        alt="loader image"
+      />
+    </main>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
-import { Suspense, lazy } from "react";
+import { Suspense } from "react";
+import Loading from "./loading";
+import dynamic from "next/dynamic";
 
-const PokemonList = lazy(() => import("@/components/ui/pokemon-list"));
+const PokemonList = dynamic(() => import("@/components/ui/pokemon-list"), {
+  ssr: false,
+  loading: () => <Loading />,
+});
 
 export default function Home() {
   return (
     <main className="w-full min-h-[100svh] flex flex-col gap-8 px-container-horizontal py-container-vertical z-0">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense>
         <PokemonList />
       </Suspense>
     </main>
